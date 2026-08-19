827ed4585e9dc70b93e25efb04db94c681e5e3ff
YuriKM2807 <yurikreuzbergmarcelino@gmail.com>
2026-08-19 14:51:39 -0300

    Arrumando Bugs (deploy perfeito no minecraft, tirando a linha 'version' dos ymls, criando 'stop.sh'

---
+++ stop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Parando todos os serviços do servidor..."
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "-> Parando Plex..."
+cd "$DIR/services/plex" && docker compose down
+
+echo "-> Parando Vaultwarden..."
+cd "$DIR/services/vaultwarden" && docker compose down
+
+echo "-> Parando Minecraft..."
+cd "$DIR/services/minecraft" && docker compose down
+
+echo "Todos os serviços foram desligados com sucesso!"
